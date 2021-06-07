bb79a18d7ae209cc4b3624d0078296333b2e304d
galpk <70628247+galpk@users.noreply.github.com>
2021-06-07 16:30:14 +0300

    Just time  in bottom right part scene builder is missing    (Everything else is done in scene builder)

--- src/View/graphs/GraphsController.java
+++ src/View/graphs/GraphsController.java
@@ -2,10 +2,21 @@ package View.graphs;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+
 
 public class GraphsController {
 
     @FXML LineChart graphleft;
     @FXML LineChart graphright;
     @FXML LineChart graphbottom;
+
+
+
+
+
+
+
+
+
 }
